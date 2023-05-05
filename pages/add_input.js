bef7d3654da9a9b4a7a396d94a3c365b66633795
RSQL001 <wizardman001@gmail.com>
2023-05-05 11:00:01 +0100

    Last commit

--- pages/add_input.js
+++ pages/add_input.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function AddInput(props) {
   const [userInput, setUserInput] = useState("");
-  
+
   const [seeItems, setSeeItems] = useState([]);
 
   const toggleSeeItem = (index) => {
@@ -15,7 +15,8 @@ export default function AddInput(props) {
       return [...prevState, index];
     });
   };
-  const handleClick = (e)=> {
+  
+  const handleClick = (e) => {
     props.addTodo(userInput, e)
     setUserInput("");
 
@@ -27,13 +28,13 @@ export default function AddInput(props) {
   return (
     <form ml-22em mt-7em>
       <input type="text"
-       value={userInput}
+        value={userInput}
         placeholder="Take down a note"
         className="block w-30 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        onChange={(e)=> setUserInput(e.target.value)} />
+        onChange={(e) => setUserInput(e.target.value)} />
       <div className="absolute inset-y-0 right-2 flex items-center">
-       
-        <button onClick={(e)=>handleClick(e)} type='submit'>
+
+        <button onClick={(e) => handleClick(e)} type='submit'>
           <a href="#" class="hover:bg-blue-400 group flex items-left rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-0 py-2 shadow-sm">
             <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
               <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
@@ -42,9 +43,9 @@ export default function AddInput(props) {
           </a>
         </button>
       </div>
-      
+
     </form>
-    
+
 
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -37,13 +37,13 @@ export default function Home() {
   }
 
   const handleOnclick = (i) => {
-   const tempArray = todos.filter((todoItem)=> {
-    console.log({todoItem: todoItem.checked})
+    const tempArray = todos.filter((todoItem) => {
+      console.log({ todoItem: todoItem.checked })
       return todoItem.checked
-  })
-    
-    
-    console.log("Enter",tempArray)
+    })
+
+
+    console.log("Enter", tempArray)
     setTodos(tempArray);
     console.log(todos);
   }
@@ -52,9 +52,7 @@ export default function Home() {
 
   return (
     //HTML Components
-    <main
-
-    >
+    <main>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <p className="fixed right-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Razaks Todo List
@@ -71,24 +69,27 @@ export default function Home() {
       </div>
 
       <div className="relative mt-5 ml-0 rounded-md shadow-sm">
-        
+
         {/* <button onClick={() => { if (value === 5 ? true : false)  (tempArray[i].checked = !tempArray[i].checked)}}>View Checked{checked ? 'uncheck' : 'check'} </button> */}
 
-        <button onClick = {handleOnclick} class=" btn w-full inline-flex items-center px-12 py-2 bg-green-600 hover:bg-green-700 text-white text-sm font-medium rounded-md">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-        </svg>
-        View Completed Tasks
-      </button>
+        <button onClick={handleOnclick} class=" btn w-full inline-flex items-center px-12 py-2 bg-green-600 hover:bg-green-700 text-white text-sm font-medium rounded-md">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+          </svg>
+          View Completed Tasks
+        </button>
 
       </div>
+      <div>
+        {
+        todos.map((todo, key) => <ShowList onChecked={onChecked} onDelete={onDelete} todo={todo} i={key} key={key} />)
+        }
+      </div>
 
-      { todos.map((todo, key) => <ShowList onChecked={onChecked} onDelete={onDelete} todo={todo} i={key} key={key} />)
-      }
 
       
 
-        {/* viewAll
+      {/* viewAll
       viewchecked
       viewUnchecked
 
